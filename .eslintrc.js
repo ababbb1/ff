9672e7c38177bf9cd53b125d1415d3d232f73942
ababbb1 <huijun.noh@sleek.kr>
2022-06-24 15:21:45 +0900

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   ],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react/jsx-props-no-spreading": "off",
     "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
   },
 };
