23178eb32182a41051192ebc1859d9c0f80c6f8d
ababbb1 <huijun.noh@sleek.kr>
2022-06-29 10:58:10 +0900

    -

--- components/followingCursor.tsx
+++ components/followingCursor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { fromEvent, merge } from 'rxjs';
 import { cls } from '../libs/client/utils';
 
