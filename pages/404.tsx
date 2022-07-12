fdb6ff6b2a3ce2cc4f4bcd6711a7bab8e68cdcc3
ababbb1 <huijun.noh@sleek.kr>
2022-07-13 00:35:47 +0900

    -

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,10 @@
-import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function E404() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/');
+  }, []);
   return null;
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    redirect: {
-      destination: '/',
-      permanent: false,
-    },
-  };
-};
